f036b952f61c43f587d9857795c4fdb4b92cf776
Yin <doyin@deloitte.com.cn>
2024-03-27 17:45:51 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,9 @@
+const toLowerCase = require('./toLowerCase')
+const numberArrayToString = require('./numberArrayToString')
+const generateUUID = require('./generateUUID')
+const isEvenNumber = require('./isEvenNumber')
+const shuffleArray = require('./shuffleArray')
+
+module.exports = (num) => {
+    return num % 2 !== 0;
+  }
